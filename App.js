7ca4229283116f6837bae722fb9c71c4a9d81d49
drizlye0 <danielkastillo03@gmail.com>
2024-03-10 14:42:19 -0500

    changes

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, View, Dimensions } from "react-native";
+
+const texasRegion = {
+  latitude: 31.9686,
+  longitude: -99.9018,
+  latitudeDelta: 10, // Ajusta este valor según el nivel de zoom deseado
+  longitudeDelta: 10,
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        provider="google"
+        initialRegion={texasRegion}
+        mapType="terrain"
+      />
     </View>
   );
 }
@@ -13,8 +25,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
