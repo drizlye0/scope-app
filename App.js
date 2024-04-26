fc493494369a3bbbe9e9312f8611801365f64a60
drizlye0 <danielkastillo03@gmail.com>
2024-04-25 23:44:54 -0500

    Adding map component and app styles

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
+import React from "react";
+import styles from "./styles";
+import Map from "./components/Map";
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Map />
+    </View>
+  );
+}
 
---
+++ styles/mapStyle.jsx
@@ -0,0 +1,38 @@
+const mapStyle = [
+  {
+    featureType: "administrative",
+    elementType: "geometry",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.icon",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    stylers: [
+      {
+        visibility: "off",
+      },
+    ],
+  },
+];
+
+export default mapStyle
