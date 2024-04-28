28348b3afd8d2b1ad3dba60dbcfd103223827d4e
drizlye0 <danielkastillo03@gmail.com>
2024-04-27 21:31:15 -0500

    Add first polygon

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import MapView from "react-native-maps";
+import MapView, { Polygon } from "react-native-maps";
 import mapStyle from "../styles/mapStyle";
 import styles from "../styles";
 
 function Map() {
+
+  const polygonCoordinates = [
+    { latitude: 25.85338150817608, longitude: -97.49098163098097, },
+    { latitude: 25.84614666134398, longitude: -97.4911093711853, },
+    { latitude: 25.847869571433534, longitude: -97.48461976647377 },
+    { latitude: 25.853146468603097, longitude: -97.4860480427742 }
+  ]
   const regionCity = {
     latitude: 25.871484185490594,
     longitude: -97.504314891994,
@@ -11,13 +18,22 @@ function Map() {
     longitudeDelta: 0.05
   }
   return (
-    <MapView
-      provider="google"
-      style={styles.map}
-      customMapStyle={mapStyle}
-      initialRegion={regionCity}
-      onPress={e => console.log(e.nativeEvent)}
-    />
+    <>
+      <MapView
+        provider="google"
+        style={styles.map}
+        customMapStyle={mapStyle}
+        initialRegion={regionCity}
+        onPress={e => console.log(e.nativeEvent)}
+      >
+        <Polygon
+          strokeColor="#ff0000"
+          fillColor="rgba(255, 41, 41, 0.1)"
+          strokeWidth={4}
+          coordinates={polygonCoordinates}
+        />
+      </MapView>
+    </>
   )
 }
 
